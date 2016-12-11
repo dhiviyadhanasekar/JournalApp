9ea83cc8322947f4c66e31a9a771191e5c744016
Dhiviya Dhanasekar (Work) <dhiviyad@yahoo-inc.com>
2016-12-11 02:10:23 -0800

    documentation

--- app/src/androidTest/java/com/dhiviyad/journalapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/dhiviyad/journalapp/ExampleInstrumentedTest.java
@@ -24,7 +24,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.dhiviyad.journalapp", appContext.getPackageName());
     }
 
--- app/src/test/java/com/dhiviyad/journalapp/ExampleUnitTest.java
+++ app/src/test/java/com/dhiviyad/journalapp/ExampleUnitTest.java
@@ -12,11 +12,6 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-//    @Test
-//    public void addition_isCorrect() throws Exception {
-//        assertEquals(4, 2 + 2);
-//    }
-
     @Test
     public void getDateFromCalendarViewDateTest() throws Exception {
         String date = DateUtils.getDateFromCalendarViewDate(2, 1, 1989);
